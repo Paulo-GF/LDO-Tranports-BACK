e06f337dca15ff99b9037c256eeaa1d75a37bacd
Your NameNeemko <thomascapo31@gmail.com>
2021-10-28 15:12:01 +0200

    Admin connection : Add cors for front connection

--- server.js
+++ server.js
@@ -15,17 +15,17 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-/* Manage session */
-const session = require("express-session");
-app.use(session({
-    secret: process.env.SECRET,
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        secure: false,
-        maxAge: (1000 * 60 * 60) // this make one hour
-    }
-}));
+// /* Manage session */
+// const session = require("express-session");
+// app.use(session({
+//     secret: process.env.SECRET,
+//     resave: true,
+//     saveUninitialized: true,
+//     cookie: {
+//         secure: false,
+//         maxAge: (1000 * 60 * 60) // this make one hour
+//     }
+// }));
 
 // // cors
 // app.use((req, res, next) => {
