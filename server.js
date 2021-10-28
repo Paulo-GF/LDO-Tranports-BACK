69ba6421d6c25d48158cf2970c65b39e71fc0b4f
Your NameNeemko <thomascapo31@gmail.com>
2021-10-28 14:35:16 +0200

    Admin connection : Add cors for front connection

--- server.js
+++ server.js
@@ -26,6 +26,20 @@ app.use(session({
       }
 }));
 
+// cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    // response to preflight request
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    }
+    else {
+      next();
+    }
+  });
+
 app.use(router);
 
 
