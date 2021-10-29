cff172700dac5151a8063aa0bdecac1a909937a9
Your NameNeemko <thomascapo31@gmail.com>
2021-10-29 18:03:41 +0200

    JWT : Add sign and verify token

--- app/middleware/authorizationMiddleware.js
+++ app/middleware/authorizationMiddleware.js
@@ -3,26 +3,30 @@ const jsonwebtoken = require('jsonwebtoken');
 const jwtSecret = process.env.JWT_SECRET;
 
 const authorization = (req, res, next) => {
-    let token;
-    if (req.headers.Authorization && req.headers.Authorization.split(' ')[0] === 'Bearer') {
-       return token = req.headers.Authorization.split(' ')[1];
-    }
-    
-    if (!token) {
-        res.status(403).json({ message: token, message2: "ahaha pas de token !" });
-        return;
-    }
-
     try {
-        const data = jsonwebtoken.verify(token, process.env.JWT_SECRET);
-        console.log(data);
-        req.userId = data.userId;
-        req.userMail = data.userMail;
-        req.userRole = data.userRole;
+
+        let token = '';
+        if (req.headers.Authorization && req.headers.Authorization.split(' ')[0] === 'Bearer') {
+            token = req.headers.Authorization.split(' ')[1];
+            console.log(token);
+
+            if (!token) {
+                return res.status(403).json({ message: token, message2: "ahaha pas de token !" });
+            } else {
+                const data = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+                console.log(data);
+                req.userId = data.userId;
+                req.userMail = data.userMail;
+                req.userRole = data.userRole;
+                next();
+            }
+        }
         
     } catch (error) {
-        res.status(403).json({ message: token, message2: "on est dans le catch"});
+        res.status(403).json({ message: token, message2: "on est dans le catch" });
     }
+
+
     // jsonwebtoken.verify(token, jwtSecret, (err, user) => {
     //     if (err) {
     //         return res.sendStatus(401);
@@ -30,7 +34,7 @@ const authorization = (req, res, next) => {
     //     req.user = user;
     //     next();
     // });
-    
+
 
 };
 
