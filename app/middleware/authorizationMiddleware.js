719ebbc55ed3cbf021a81f2471b59e1a22a1fe20
Your NameNeemko <thomascapo31@gmail.com>
2021-10-29 16:36:18 +0200

    JWT : Add sign and verify token

--- app/middleware/authorizationMiddleware.js
+++ app/middleware/authorizationMiddleware.js
@@ -1,23 +1,26 @@
 const jsonwebtoken = require('jsonwebtoken');
 
+const jwtSecret = process.env.JWT_SECRET;
+
 const authorization = (req, res, next) => {
     console.log(req)
     let token;
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-        token = req.headers.authorization.split(' ')[1];
-       }
+       return token = req.headers.authorization.split(' ')[1];
+    }
+
     if (!token) {
-      return res.status(403).json({message : token, message2: "ahaha pas de token !"});
+        return res.status(403).json({ message: token, message2: "ahaha pas de token !" });
     }
-    try {
-      const data = jsonwebtoken.verify(token, jwtSecret);
-      req.userId = data.userId;
-      req.userMail = data.userMail;
-      req.userRole = data.userRole;
-      return next();
-    } catch {
-      return res.status(403).json({message : token});
-    }
-  };
+
+    const data = jsonwebtoken.verify(token, jwtSecret);
+    console.log(data);
+    req.userId = data.userId;
+    req.userMail = data.userMail;
+    req.userRole = data.userRole;
+    
+    next();
+
+};
 
 module.exports = authorization;
