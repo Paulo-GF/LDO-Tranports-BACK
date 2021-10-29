80d68991bbbdc5a6eac098dceeec2d5458b9c61b
Your NameNeemko <thomascapo31@gmail.com>
2021-10-29 13:57:52 +0200

    JWT : Add sign and verify token

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -1,3 +1,4 @@
+const jsonwebtoken = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const userModel = require('../models/user');
@@ -13,8 +14,8 @@ const adminController = {
      */
     adminSignin: async function (req, res) {
 
-        const form = req.body;
-        const user = await userModel.getUser(form);
+        const { mail, password } = req.body;
+        const user = await userModel.getUser(mail, password);
         // console.log(form);
         // console.log('##### User avant test');
         // console.log(user);
@@ -22,22 +23,27 @@ const adminController = {
         // If mail is not allowed
         if (!user) {
             // Send a new error 403 - forbidden
-            return res.json(`Accès refusé, le mail ${form.mail} n'est pas autorisé`);
+            return res.json(`Accès refusé, le mail ${mail} n'est pas autorisé`);
 
         }
         // If mail is allowed, check password hash and form password
-        if (user !== undefined && !bcrypt.compareSync(form.password, user.hash)) {
+        if (user !== undefined && !bcrypt.compareSync(password, user.hash)) {
             // If mail is ok but password isn't ok : send error 403 - forbidden
-            return res.json(`Accès refusé, le mot de passe ${form.password} n'est pas autorisé`);
+            return res.json(`Accès refusé, le mot de passe ${password} n'est pas autorisé`);
 
         }
-       
+
+
+        const token = jsonwebtoken.sign({ userId: user.id, userMail: user.mail, userRole : user.role }, jwtSecret);
+        res.cookie('access_token', token, { httpOnly: true });
+
         // password is ok and mail is ok
         res.json({
             message: `Connection de l'utilisateur ${user.firstname} : établie !`,
             userId: user.id,
             userFirstName: user.firstname,
-            role: user.role
+            role: user.role,
+            token : token
         });
 
 
--- app/router.js
+++ app/router.js
@@ -1,12 +1,20 @@
-const {Router} = require('express');
-
+const { Router } = require('express');
 const router = new Router();
 
 const adminController = require('./controllers/adminController');
+const authorizationMiddleware = require('./middleware/authorizationMiddleware')
+
 
 // Router - Admin Connection
 router.post('/admin-signin', adminController.adminSignin);
-router.post('/admin-logged/:adminId', adminController.modifyPassword);
+router.post('/admin-logged/:adminId', authorizationMiddleware, adminController.modifyPassword);
+
+router.get("/logout", authorizationMiddleware, (_, res) => {
+    return res
+        .clearCookie("access_token")
+        .status(200)
+        .json({ message: "Successfully logged out 😏 🍀" });
+});
 
 
 module.exports = router;
