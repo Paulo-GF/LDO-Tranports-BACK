a954d94e223bf5be04e04cb50c6359a04cf97511
Your NameNeemko <thomascapo31@gmail.com>
2021-10-29 18:20:36 +0200

    JWT : Add sign and verify token

--- app/middleware/authorizationMiddleware.js
+++ app/middleware/authorizationMiddleware.js
@@ -9,34 +9,23 @@ const authorization = (req, res, next) => {
         if (req.headers.Authorization && req.headers.Authorization.split(' ')[0] === 'Bearer') {
             let token = req.headers.Authorization.split(' ')[1];
             console.log(token);
+
+            if (!token) {
+                return res.status(403).json({ message: token, message2: "ahaha pas de token !" });
+            } 
+
             const data = jsonwebtoken.verify(token, process.env.JWT_SECRET);
             console.log(data);
             req.userId = data.userId;
             req.userMail = data.userMail;
             req.userRole = data.userRole;
         }
-
-        // if (!token) {
-        //     return res.status(403).json({ message: token, message2: "ahaha pas de token !" });
-        // } 
-
-        
-        
         
     } catch (error) {
         res.status(403).json({ message: token, message2: "on est dans le catch" });
     }
     
     next();
-
-    // jsonwebtoken.verify(token, jwtSecret, (err, user) => {
-    //     if (err) {
-    //         return res.sendStatus(401);
-    //     }
-    //     req.user = user;
-    //     next();
-    // });
-
 
 };
 
