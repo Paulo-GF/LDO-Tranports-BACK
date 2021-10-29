5fa5dbc0c8f5fd7647a3b17bb78681bf48022a9b
Your NameNeemko <thomascapo31@gmail.com>
2021-10-29 19:04:36 +0200

    JWT : Add sign and verify token

--- app/middleware/authorizationMiddleware.js
+++ app/middleware/authorizationMiddleware.js
@@ -3,50 +3,23 @@ const jsonwebtoken = require('jsonwebtoken');
 const jwtSecret = process.env.JWT_SECRET;
 
 const authorization = (req, res, next) => {
-    try {
-
-
-        if (req.headers.Authorization && req.headers.Authorization.split(' ')[0] === 'Bearer') {
-            let token = req.headers.Authorization.split(' ')[1];
-            console.log(token);
-
-            if (!token) {
-                return res.status(403).json({ message: token, message2: "ahaha pas de token !" });
-            }
-
-            // const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET);
-            // console.log(decoded);
-
-            
-            jsonwebtoken.verify(token, process.env.JWT_SECRET, function (err, decoded) {
-                if (err) {
-                    return res.status(401).json('token_not_valid');
-                } else {
-                    req.decoded = decoded;
-                    next();
-                }
-
-            });
-
-            // req.userId = data.userId;
-            // req.userMail = data.userMail;
-            // req.userRole = data.userRole;
-
-            // jsonwebtoken.verify(token, jwtSecret, (err, user) => {
-            //     console.log(user);
-            //     if (err) {
-            //         return res.sendStatus(401);
-            //     }
-            //     req.user = user;
-            //     next();
-            // });
-        }
-
-    } catch (error) {
-        res.status(403).json({ message: token, message2: "on est dans le catch" });
+    let token = req.headers['x-access-token'] || req.headers['authorization'];
+    if (!!token && token.startsWith('Bearer ')) {
+        token = token.slice(7, token.length);
     }
 
-    //next();
+    if (token) {
+        jsonwebtoken.verify(token, jwtSecret, (err, decoded) => {
+            if (err) {
+                return res.status(401).json('token_not_valid');
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        return res.status(401).json('token_required');
+    }
 
 };
 
