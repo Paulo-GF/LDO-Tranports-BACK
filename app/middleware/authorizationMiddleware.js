3ecb1dcaffbc2cf335d55894233600e454093c05
Your NameNeemko <thomascapo31@gmail.com>
2021-10-29 18:57:55 +0200

    JWT : Add sign and verify token

--- app/middleware/authorizationMiddleware.js
+++ app/middleware/authorizationMiddleware.js
@@ -5,38 +5,48 @@ const jwtSecret = process.env.JWT_SECRET;
 const authorization = (req, res, next) => {
     try {
 
-        
+
         if (req.headers.Authorization && req.headers.Authorization.split(' ')[0] === 'Bearer') {
             let token = req.headers.Authorization.split(' ')[1];
             console.log(token);
 
             if (!token) {
                 return res.status(403).json({ message: token, message2: "ahaha pas de token !" });
-            } 
+            }
 
-            // const data = jsonwebtoken.verify(token, process.env.JWT_SECRET);
-            // console.log(data);
+            // const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+            // console.log(decoded);
+
+            
+            jsonwebtoken.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+                if (err) {
+                    return res.status(401).json('token_not_valid');
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
+
+            });
+
             // req.userId = data.userId;
             // req.userMail = data.userMail;
             // req.userRole = data.userRole;
-            jsonwebtoken.verify(token, jwtSecret, (err, user) => {
-                console.log(user);
-                if (err) {
-                    return res.sendStatus(401);
-                }
-                req.user = user;
-                next();
-            });
+
+            // jsonwebtoken.verify(token, jwtSecret, (err, user) => {
+            //     console.log(user);
+            //     if (err) {
+            //         return res.sendStatus(401);
+            //     }
+            //     req.user = user;
+            //     next();
+            // });
         }
-        
+
     } catch (error) {
         res.status(403).json({ message: token, message2: "on est dans le catch" });
     }
-    
+
     //next();
-
-    
-
 
 };
 
