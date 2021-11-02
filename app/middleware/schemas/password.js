67c1d8eca9ada7c8bfb1a065598c6fe9689ca715
Paulo-GF <benfiquistagf@gmail.com>
2021-11-02 15:15:09 +0100

    Joi creation : setup passwordSchema + jobSchema + validatorSchema

--- app/middleware/schemas/password.js
+++ app/middleware/schemas/password.js
@@ -1,6 +1,7 @@
 const Joi = require('joi');
 
 const passwordSchema = Joi.object({
+    userId : Joi.number(),
     newPassword : Joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,24}$/).required(),
     newPasswordConfirm : Joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,24}$/).required()
 });
