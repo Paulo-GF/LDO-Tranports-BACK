0393dc3a3f95653ada62dcdd75416aa6b9139660
Paulo-GF <benfiquistagf@gmail.com>
2021-11-02 15:10:04 +0100

    Joi creation : setup passwordSchema + jobSchema + validatorSchema

--- app/middleware/validator.js
+++ app/middleware/validator.js
@@ -7,7 +7,7 @@ const validatorModule = {
 
             if(error){
                 console.log("cette saisie n'est pas conforme",error);
-                res.status(400).json({message : "cette saisie n'est pas conforme", error: error.detail});
+                res.status(400).json({message : "cette saisie n'est pas conforme", error});
             }
             else{
                 next();
