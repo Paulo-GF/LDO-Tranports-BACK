949d05baf7b43b28004e368ee4bb98a9f833592f
Your NameNeemko <thomascapo31@gmail.com>
2021-11-03 11:55:29 +0100

    JOB API : Test update with front

--- app/middleware/validator.js
+++ app/middleware/validator.js
@@ -2,8 +2,9 @@ const validatorModule = {
     // example : validatorModule.isCorrect(addJobSchema),
     isCorrect:(schema)=>{
         return (req,res,next)=>{
-           
+           console.log(schema);
             const {error} = schema.validate(req.body);
+            console.log(req.body);
 
             if(error){
                 console.log("cette saisie n'est pas conforme",error);
