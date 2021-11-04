c71d3e850b22c2945b1383fa5325ea02b8883627
Your NameNeemko <thomascapo31@gmail.com>
2021-11-04 11:00:33 +0100

    Nodemailer : setup nodemailer with multer for manage attachements

--- app/middleware/uploadFiles.js
+++ app/middleware/uploadFiles.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../uploads/')
+        cb(null, 'uploads/')
     },
 
     // on récupére le nom du fichier pour le stocker
