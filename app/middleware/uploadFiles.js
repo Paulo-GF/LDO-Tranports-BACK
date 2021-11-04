d107977f660ee7dd2ae0c7bda68a94e1c3ab0456
Your NameNeemko <thomascapo31@gmail.com>
2021-11-04 11:57:13 +0100

    Nodemailer : setup nodemailer with multer for manage attachements

--- app/middleware/uploadFiles.js
+++ app/middleware/uploadFiles.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage: storage }).single('file');
+const upload = multer({ storage: storage }).array('file');
 
 //next();
 
--- server.js
+++ server.js
@@ -32,7 +32,6 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static('uploads'));
 
 //const jwtSecret = process.env.JWT_SECRET;
 
