113dbe43ad757bd5a6f01b940cbf7f835f85eb1b
Your NameNeemko <thomascapo31@gmail.com>
2021-11-08 11:24:56 +0100

    Contact Page : add prettier mail template

--- app/controllers/contactController.js
+++ app/controllers/contactController.js
@@ -26,7 +26,7 @@ const contactController = {
                 from: `"Email:" <${req.body.userMail}>`, // sender address
                 to: 'ldonodemailer@gmail.com', // list of receivers
                 subject: req.body.subject, // Subject line
-                html: `<div style="opacity: 0.9; background-repeat: no-repeat;; background-image: url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1744&q=80'); box-sizing: border-box; border-radius: 15px; width: 60vw; height: 80vh;">
+                html: `<div style="opacity: 0.9; background-repeat: no-repeat;; background-image: url('https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1744&q=80'); box-sizing: border-box; border-radius: 15px; width: 50vw; height: 80vh;">
     
                 <div style="color: white; font-size: 1.9em; font-weight: bold; padding: 2em 2em 1em;"> Vous avez reçu une demande de contact à propos de : <span style="font-weight: bold; background-color: rgba(64, 194, 211, 0.589); border-radius: 10px; padding: 5px;">${req.body.subject}</span> </div>
                 
