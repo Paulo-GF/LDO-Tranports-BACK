bdf9f454e147dde61bcc362166d2b7bd3c9b490d
Your NameNeemko <thomascapo31@gmail.com>
2021-11-11 12:00:35 +0100

    JOI : UpdateJob Schema modifications and add contact schema and apply schema

--- app/middleware/schemas/apply.js
+++ app/middleware/schemas/apply.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 // Schema description when someone applied for a job
 const applySchema = Joi.object({
     // Data validator :
+    jobId: Joi.number(),
     userMail : Joi.string().pattern(/^[^@\s]+@[^@\s]+\.[^@\s]+$/).required(),
     firstName : Joi.string().required(),
     lastName : Joi.string().required(),
